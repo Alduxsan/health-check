b28fc56f0df46f9abecc50da23d99462e5858e50
Alduxsan <aldosantarelli@gmail.com>
2020-09-19 11:06:55 -0300

    Renombre las variable min_absolute a min_GB y agregue nombre de variable en la invocacion de la funcion

--- check_free_space.py
+++ check_free_space.py
@@ -3,19 +3,19 @@
 import shutil
 import sys
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_disk_usage(disk, min_GB, min_percent):
 	"""	Returns True if there is enough free disk space."""
 	du = shutil.disk_usage(disk)
 	#calculate the percentage of free space
 	percent_free = 100 * du.free / du.total
 	#calculate how many free gigabytes
 	gigabytes_free = du.free / 2**30
-	if percent_free < min_percent or gigabytes_free < min_absolute:
+	if percent_free < min_percent or gigabytes_free < min_GB:
 		return False
 	return True
 
 #check for at least 2 GB and 10% free
-if not check_disk_usage("/", 2, 10):
+if not check_disk_usage(disk="/",min_GB=2,min_percent=10):
 	print("ERROR: No hay suficiente espacio en disco.")
 	sys.exit(1)
 
